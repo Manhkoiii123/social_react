f1e054e6ffd73be4381ddc828214550bb47cb127
manhtranduc <manhtranduc0202@gmail.com>
2025-05-20 16:51:18 +0700

    rtk query

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,27 +1,71 @@
 import FormField from "@components/FormField";
 import TextInput from "@components/FormInputs/TextInput";
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
+import { useRegisterMutation } from "@services/rootApi";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const RegisterPage = () => {
-  const { control } = useForm();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [register, { data = {}, isLoading, error, isError, isSuccess }] =
+    useRegisterMutation();
+
+  const formSchema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup
+      .string()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Email is not valid",
+      )
+      .required(),
+    password: yup.string().required(),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  function onSubmit(formData) {
+    console.log({ formData });
+    register(formData);
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      // dispatch(openSnackbar({ message: data.message }));
+      navigate("/login");
+    }
+  }, [isSuccess, data.message, navigate, dispatch]);
+
+  console.log({ data, isLoading, error, errors });
 
   return (
     <div>
       <p className="mb-5 text-center text-2xl font-bold">Register</p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           name="fullName"
           label="Full Name"
           control={control}
           Component={TextInput}
+          error={errors["fullName"]}
         />
         <FormField
           name="email"
           label="Email"
           control={control}
           Component={TextInput}
+          error={errors["email"]}
         />
         <FormField
           name="password"
@@ -29,8 +73,12 @@ const RegisterPage = () => {
           control={control}
           type="password"
           Component={TextInput}
+          error={errors["password"]}
         />
-        <Button variant="contained">Sign up</Button>
+        <Button variant="contained" type="submit">
+          Sign up
+        </Button>
+        {isError && <Alert severity="error">{error?.data?.message}</Alert>}
       </form>
       <p className="mt-4">
         Already have an account? <Link to="/login">Sign in instead</Link>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import { rootApi } from "@services/rootApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [rootApi.reducerPath]: rootApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(rootApi.middleware),
 });
