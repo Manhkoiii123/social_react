09fd73bd2382aa6094745494ff26b79fd75ebe03
manhtranduc <manhtranduc0202@gmail.com>
2025-07-17 21:03:34 +0700

    register

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,14 +1,31 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
-// Supports weights 100-900
-import '@fontsource-variable/public-sans';
+import "@fontsource-variable/public-sans";
+import { Alert, Snackbar } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { closeSnackbar } from "@redux/slices/snackbarSlice";
 
 const RootLayout = () => {
+  const dispatch = useDispatch();
+
+  const { open, type, message } = useSelector((state) => {
+    return state.snackbar;
+  });
+
   return (
     <div>
       <Suspense fallback={<p>Loading</p>}>
         <Outlet />
       </Suspense>
+      <Snackbar
+        open={open}
+        autoHideDuration={4000}
+        onClose={() => dispatch(closeSnackbar())}
+      >
+        <Alert severity={type} variant="filled" sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,27 +1,53 @@
 import FormField from "@components/FormField";
 import TextInput from "@components/FormInputs/TextInput";
-import { Button } from "@mui/material";
+import { Alert, Button } from "@mui/material";
+import { openSnackbar } from "@redux/slices/snackbarSlice";
+import { useRegisterMutation } from "@services/rootApi";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
-  const { control } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    // resolver: yupResolver(formSchema),
+  });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [register, { data = {}, error, isError, isSuccess }] =
+    useRegisterMutation();
+  function onSubmit(formData) {
+    console.log({ formData });
+    register(formData);
+  }
 
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(openSnackbar({ message: data.message }));
+      navigate("/login");
+    }
+  }, [isSuccess, data.message, navigate, dispatch]);
   return (
     <div>
       <p className="mb-5 text-center text-2xl font-bold">Register</p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           name="fullName"
           label="Full Name"
           control={control}
           Component={TextInput}
+          error={errors["fullName"]}
         />
         <FormField
           name="email"
           label="Email"
           control={control}
           Component={TextInput}
+          error={errors["email"]}
         />
         <FormField
           name="password"
@@ -29,8 +55,12 @@ const RegisterPage = () => {
           control={control}
           type="password"
           Component={TextInput}
+          error={errors["password"]}
         />
-        <Button variant="contained">Sign up</Button>
+        <Button variant="contained" type="submit">
+          Sign up
+        </Button>
+        {isError && <Alert severity="error">{error?.data?.message}</Alert>}
       </form>
       <p className="mt-4">
         Already have an account? <Link to="/login">Sign in instead</Link>
