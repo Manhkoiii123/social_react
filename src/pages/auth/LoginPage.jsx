49e1e52860f15fed92d0ed0dffea21fabac5af46
manhtranduc <manhtranduc0202@gmail.com>
2025-07-17 21:11:27 +0700

    login

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,21 +1,73 @@
 import FormField from "@components/FormField";
 import TextInput from "@components/FormInputs/TextInput";
-import { Button } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, CircularProgress } from "@mui/material";
+import { openSnackbar } from "@redux/slices/snackbarSlice";
+import { useLoginMutation } from "@services/rootApi";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
 
 const LoginPage = () => {
-  const { control } = useForm();
+  const [login, { data = {}, isLoading, error, isError, isSuccess }] =
+    useLoginMutation();
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const formSchema = yup.object().shape({
+    email: yup
+      .string()
+      .matches(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Email is not valid",
+      )
+      .required(),
+    password: yup.string().required(),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  function onSubmit(formData) {
+    login(formData);
+  }
+
+  console.log({ email: getValues("email") });
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(openSnackbar({ type: "error", message: error?.data?.message }));
+    }
+
+    if (isSuccess) {
+      dispatch(openSnackbar({ message: data.message }));
+      navigate("/verify-otp", {
+        state: {
+          email: getValues("email"),
+        },
+      });
+    }
+  }, [isError, error, dispatch, data.message, isSuccess, navigate, getValues]);
 
   return (
     <div>
       <p className="mb-5 text-center text-2xl font-bold">Login</p>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <FormField
           name="email"
           label="Email"
           control={control}
           Component={TextInput}
+          error={errors["email"]}
         />
         <FormField
           name="password"
@@ -23,11 +75,15 @@ const LoginPage = () => {
           control={control}
           type="password"
           Component={TextInput}
+          error={errors["password"]}
         />
-        <Button variant="contained">Sign in</Button>
+        <Button variant="contained" type="submit">
+          {isLoading && <CircularProgress size="16px" className="mr-1" />}
+          Sign in
+        </Button>
       </form>
       <p className="mt-4">
-        New on our platform? <Link to="/login">Create an account</Link>
+        New on our platform? <Link to="/register">Create an account</Link>
       </p>
     </div>
   );
